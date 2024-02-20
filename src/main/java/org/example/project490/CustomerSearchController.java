9e7d494eca497ca93cd613a11b185bbe785429cd
TheftJord <voltstar78@gmail.com>
2024-02-20 15:49:49 -0500

    Edmund
    I messed up a little bit and put it on the main branch but I don't think this will cause any issues

--- src/main/java/org/example/project490/CustomerSearchController.java
+++ src/main/java/org/example/project490/CustomerSearchController.java
@@ -2,8 +2,10 @@ package org.example.project490;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
 
 /**
  *
@@ -25,6 +27,8 @@ public class CustomerSearchController {
     
     @FXML
     private ComboBox customerSelectComboBox;
+
+    Alert a=new Alert(AlertType.ERROR);
     
     /**
      * event handler for home page menu button
@@ -60,6 +64,37 @@ public class CustomerSearchController {
     @FXML
     private void closeGUI() {
         System.exit(0);
+    }
+
+    /**
+     * event handler for search button
+     * will give information to the search engine
+     */
+    @FXML
+    private void engageSearch(){
+        String firstName=firstNameSearchTextField.getText();
+        String lastName=lastNameSearchTextField.getText();
+        String address=addressSearchTextField.getText();
+        String phoneNumber=phoneSearchTextField.getText();
+        if(firstName!=null&&lastName!=null){
+            SearchEngine.customerSearch(firstName,lastName);
+        }
+        else if(address!=null){
+            SearchEngine.customerSearch(address);
+        }
+        else if(phoneNumber!=null){
+            SearchEngine.customerSearch(phoneNumber);
+        }
+        else{
+            a.show();
+        }
+    }
+
+    @FXML
+    private void updateComboBox(){
+        customerSelectComboBox.isResizable();
+        
+        customerSelectComboBox.setItems(null);
     }
     
 }
--- src/main/java/org/example/project490/SearchEngine.java
+++ src/main/java/org/example/project490/SearchEngine.java
@@ -4,14 +4,14 @@ public class SearchEngine extends Thread{
     /**
      * universal variables that will be used as the values to search through the database
      */
-    int idNumber;
+    static int idNumber;
 
     /**
      * @param firstName
      * @param lastName
      * @return
      */
-    public int customerSearch(String firstName, String lastName) {
+    public static int customerSearch(String firstName, String lastName) {
         String fName="";
         String lName="";
         idNumber=0;
@@ -27,7 +27,7 @@ public class SearchEngine extends Thread{
      * @param address
      * @return
      */
-    public int customerSearch(String address){
+    public static int customerSearch(String address){
         String placeOfLiving="";
         idNumber=0;
         while(address!=placeOfLiving){
@@ -41,7 +41,7 @@ public class SearchEngine extends Thread{
      * @param phoneNumber
      * @return
      */
-    public int customerSearch(String phoneNumber){
+    public static int customerSearch(String phoneNumber){
         String pNumber="";
         idNumber=0;
         while(phoneNumber!=pNumber){
@@ -49,5 +49,48 @@ public class SearchEngine extends Thread{
             idNumber=0;
         }
         return idNumber;
+    }
+
+    /**
+     * this turns strings into char arrays
+     * this makes it possible to search letter by letter
+     * @param item
+     * @return
+     */
+    public char[] characterByCharacter(String item){
+        int x=item.length();
+        char letter[]=new char[x];
+        for(int i=0;i<item.length();i++){
+            letter[i]=item.charAt(i);
+        }
+        return letter;
+    }
+
+    /**
+     * this compares two different char[] by which one is longer
+     * this will return how many have matched betweent the two char[]
+     * this will be used to search for the matches
+     * @param searchQuery
+     * @param databaseEntry
+     * @return
+     */
+    public int compareStrings(char[] searchQuery, char[] databaseEntry){
+        int compareLength=0;
+        int similarityCount=0;
+        if(searchQuery.length>databaseEntry.length){
+            compareLength=searchQuery.length;
+        }
+        else if(databaseEntry.length>searchQuery.length){
+            compareLength=databaseEntry.length;
+        }
+        else{
+            System.out.printf("Error has occured at SearchEngine.compareString() \n");
+        }
+        for(int i=0;i<compareLength;i++){
+            if(searchQuery[i]==databaseEntry[i]){
+                similarityCount++;
+            }
+        }
+        return similarityCount;
     }
 }
